b90561553fce01374921b7db9a74b9d3e7e314fc
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-11-05 22:04:11 +0300

    Vers 1.0

--- gram.py
+++ gram.py
@@ -1,1 +1,10 @@
-print('asdasd****sa')
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "7700680355:AAFHfbUpjkYIqlIdWxWM4QbIsffcrvdYkHY"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)

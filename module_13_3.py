4d6997743b8ae7ea2d1b6305aad3ffac71ed6bf2
ilchenkokostya <ilchenko.kostya@gmail.com>
2024-11-09 22:29:56 +0300

    Vers 1.0

--- module_13_3.py
+++ module_13_3.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import asyncio
 
-api = "7700680355:AAFHfbUpjkYIqlIdWxWM4QbIsffcrvdYkHY"
+api = ""
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
